1d4ebc5e63c00ef558a3af46419fde26989789e8
decaduto <78024779+decaduto@users.noreply.github.com>
2021-12-10 19:19:18 +0100

    updated to v. 1.26
    fixed problems with I/O functions

--- wlan_modules/realtek/magma_realtek.h
+++ wlan_modules/realtek/magma_realtek.h
@@ -22,6 +22,10 @@
         short int write_res;
     }magma_realtek_io_res;
 
+    struct magma_realtek_usb{
+
+    };
+
     u8 magma_realtek_usb_read8(u32 offset);
     u16 magma_realtek_usb_read16(u32 offset);
     u32 magma_realtek_usb_read16(u32 offset);
@@ -34,7 +38,7 @@
         int status = 0;
         void *data_buffer = kmalloc(8, GFP_USER);
         /* where 'x' is the devnum */
-        int pipe = ( ( 2 << 30 ) | ( x << 8 ) | 0 | 0x80 );
+        int pipe = ( ( 2 << 30 ) | ( magma_realtek_usb->devnum << 8 ) | 0 | 0x80 );
         status = usb_control_msg(magma_realtek_usb, pipe, 0x05, 0xC0, (u16)(offset & 0x0000ffff), 0, data_buffer, 1, 500);
         memcpy(dumped_buffer_data, data_buffer, status);
         return (u8)(le32_to_cpu(dumped_buffer_data) & 0x0ff);
@@ -44,7 +48,7 @@
         int status = 0;
         void *data_buffer = kmalloc(16, GFP_USER);
         /* where 'x' is the devnum */
-        int pipe = ( ( 2 << 30 ) | ( x << 8 ) | 0 | 0x80 );
+        int pipe = ( ( 2 << 30 ) | ( magma_realtek_usb->devnum << 8 ) | 0 | 0x80 );
         status = usb_control_msg(magma_realtek_usb, pipe, 0x05, 0xC0, (u16)(offset & 0x0000ffff), 0, data_buffer, 2, 500);
         memcpy(dumped_buffer_data, data_buffer, status);
         return (u16)(le32_to_cpu(dumped_buffer_data) & 0xffff);
@@ -54,7 +58,7 @@
         int status = 0;
         void *data_buffer = kmalloc(32, GFP_USER);
         /* where 'x' is the devnum */
-        int pipe = ( ( 2 << 30 ) | ( x << 8 ) | 0 | 0x80 );
+        int pipe = ( ( 2 << 30 ) | ( magma_realtek_usb->devnum << 8 ) | 0 | 0x80 );
         status = usb_control_msg(magma_realtek_usb, pipe, 0x05, 0xC0, (u16)(offset & 0x0000ffff), 0, data_buffer, 4, 500);
         memcpy(dumped_buffer_data, data_buffer, status);
     }
@@ -62,30 +66,36 @@
     void magma_realtek_usb_write8(u32 offset, u8 value_to_write){
         __le32 data;
         int status = 0;
+	    u8 *palloc_buf, *pIo_buf;
+        int pipe = ( ( 2 << 30 ) | ( magma_realtek_usb->devnum << 8 ) | 0  );
         data = cpu_to_le32((u32)value_to_write & 0x000000ff);
+        palloc_buf = kmalloc((u32)1 + 16, GFP_ATOMIC);
+	    pIo_buf = palloc_buf + 16 - ((addr_t)(palloc_buf) & 0x0f);
         /* retake from here: https://elixir.bootlin.com/linux/latest/source/drivers/staging/rtl8712/usb_ops_linux.c#L470 */
-        /* no buffer is needed for the write operations */
-        usb_control_msg();
+        memcpy(pIo_buf, &data, 1);
+        usb_control_msg(magma_realtek_usb, pipe, 0x05, 0x40, (u16)(offset & 0x0000ffff), 0, &data, 1, 500);
 
     }
 
     void magma_realtek_usb_write16(u32 offset, u16 value_to_write){
         __le32 data;
         int status = 0;
-        data = cpu_to_le32((u32)value_to_write & 0x000000ff);
+        int pipe = ( ( 2 << 30 ) | ( magma_realtek_usb->devnum << 8 ) | 0  );
+        data = cpu_to_le32((u32)value_to_write & 0x0000ffff);
         /* retake from here: https://elixir.bootlin.com/linux/latest/source/drivers/staging/rtl8712/usb_ops_linux.c#L470 */
         /* no buffer is needed for the write operations */
-        usb_control_msg();
+        usb_control_msg(magma_realtek_usb, pipe, 0x05, 0x40, (u16)(offset & 0x0000ffff), 0, &data, 2, 500);
 
     }
 
     void magma_realtek_usb_write32(u32 offset, u32 value_to_write){
         __le32 data;
         int status = 0;
-        data = cpu_to_le32((u32)value_to_write & 0x000000ff);
+        int pipe = ( ( 2 << 30 ) | ( magma_realtek_usb->devnum << 8 ) | 0  );
+        data = cpu_to_le32((u32)value_to_write & 0x0000ffff);
         /* retake from here: https://elixir.bootlin.com/linux/latest/source/drivers/staging/rtl8712/usb_ops_linux.c#L470 */
         /* no buffer is needed for the write operations */
-        usb_control_msg();
+        usb_control_msg(magma_realtek_usb, pipe, 0x05, 0x40, (u16)(offset & 0x0000ffff), 0, &data, 4, 500);
     }
 
     struct magma_realtek_io_res *magma_realtek_main_io(enum magma_realtek_available_io_ops, u16 offset, u32 value_to_write){
