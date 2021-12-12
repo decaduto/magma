025f5d126cf8ce4d147b9e92a47615c837b5258a
decaduto <78024779+decaduto@users.noreply.github.com>
2021-12-12 01:01:27 +0100

    added generic hard/soft mac functions, those will be included in MagMa-v.c if and only if the detected adapter will be softmac || hardmac

--- wlan_modules/generic_softmac/generic.h
+++ wlan_modules/generic_softmac/generic.h
@@ -1,6 +1,43 @@
 /*  © 2021 Edoardo Mantovani All Rights Reserved */
-#ifndef __GENERIC_SOFTMAC_H__
-    #define __GENERIC_SOFTMAC_H__
+#ifndef __GENERIC_HARDMAC_H__
+    #define __GENERIC_HARDMAC_H__
+
+    struct wireless_dev *magma_add_interface(struct wiphy *wiphy, const char *name, unsigned char name_assign_type, enum nl80211_iftype type, struct vif_params *params );
+    int magma_del_interface(struct wiphy *wiphy, struct wireless_dev *wdev);
+    int magma_start_ap_mode(struct wiphy *wiphy, struct net_device *dev, struct cfg80211_ap_settings *settings);
+    int magma_stop_ap_mode(struct wiphy *wiphy, struct net_device *dev);
+    int magma_scan(struct wiphy *wiphy, struct cfg80211_scan_request *request);
+
+
+    struct wireless_dev *magma_add_interface(struct wiphy *wiphy, const char *name, unsigned char name_assign_type, enum nl80211_iftype type, struct vif_params *params ){
+
+
+
+    }
+
+
+    int magma_del_interface(struct wiphy *wiphy, struct wireless_dev *wdev){
+
+
+    }
+
+    int magma_start_ap_mode(struct wiphy *wiphy, struct net_device *dev, struct cfg80211_ap_settings *settings){
+
+
+    }
+
+    int magma_stop_ap_mode(struct wiphy *wiphy, struct net_device *dev){
+
+
+
+    }
+
+    int magma_scan(struct wiphy *wiphy, struct cfg80211_scan_request *request){
+
+
+
+    }
+
 
 
 
